33bc5d5115eb4c239070a11d152814ff24022c9b
Nicole Li <nicolehcli@gmail.com>
2018-10-05 17:50:43 -0700

    Saving progress

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -32,7 +32,16 @@ class RegisterViewController: UIViewController {
 
         
         //TODO: Set up a new user on our Firbase database
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            //check if error is sent back
+            if error != nil {
+                print(error!)
+            } else {
+                // success
+                print ("Registration successful")
+            }
+        }
         
 
         
